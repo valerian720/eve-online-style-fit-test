be7d62b47d9e741941e0e9c76b5fd9b5ffba5bd3
Валериан Воротников <valerian7202@gmail.com>
2023-05-06 19:37:37 +0300

    base

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,128 +1,69 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa"
-          target="_blank"
-          rel="noopener"
-          >pwa</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
-          target="_blank"
-          rel="noopener"
-          >vuex</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest"
-          target="_blank"
-          rel="noopener"
-          >unit-jest</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-e2e-cypress"
-          target="_blank"
-          rel="noopener"
-          >e2e-cypress</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
+  <div class="">
+    <div class="px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+      <h1 class="display-4">Баллансировка</h1>
+      <p class="lead">
+        Выберите в левой колонке пункты и их количество что бы в правой колонке
+        появился итоговый балланс
+      </p>
+      <button class="btn btn-lg btn-block btn-outline-primary" @click="clickMe">
+        Подсчитать
+      </button>
+    </div>
+
+    <div class="row m-5 p-1">
+      <div class="col m-1 p-1">
+        <div class="card mb-4 box-shadow">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Элементы</h4>
+          </div>
+          <div class="card-body">
+            <ul class="list-unstyled mt-3 mb-4">
+              <li v-for="(curArmor, index) in armor" :key="index">
+                <ul>
+                  <li v-for="(data, dataName) in curArmor" :key="dataName">
+                    {{ dataName }}: {{ data }}
+                  </li>
+                </ul>
+                <div class="row">
+                  <div class="col">
+                    <button
+                      type="button"
+                      @click="curArmor.IncreaseCount()"
+                      class="btn btn-primary m-1 col-1"
+                    >
+                      +
+                    </button>
+                    <button
+                      type="button"
+                      @click="curArmor.DecreaseCount()"
+                      class="btn btn-primary m-1 col-1"
+                    >
+                      -
+                    </button>
+                  </div>
+                </div>
+                --------------
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="col m-1 p-1">
+        <div class="card mb-4 box-shadow">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Итоговый баланс</h4>
+          </div>
+          <div class="card-body">
+            <ul class="list-unstyled mt-3 mb-4">
+              <li v-for="(data, index) in displayedStatistics" :key="index">
+                {{ index }} : {{ data.toFixed(2) }}
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -136,19 +77,108 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="less">
-h3 {
-  margin: 40px 0 0;
+<style scoped lang="less"></style>
+<!-- 
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+// i need a class to store stats
+class Armor {
+  name: string;
+  healthBoost: number;
+  damageBoost: number;
+  speedBoost: number;
+
+  amountSelected: number;
+
+  constructor(
+    name: string,
+    healthBoost: number,
+    damageBoost: number,
+    speedBoost: number
+  ) {
+    this.amountSelected = 0;
+    this.name = name;
+    //
+    [this.healthBoost, this.damageBoost, this.speedBoost] = [
+      healthBoost,
+      damageBoost,
+      speedBoost,
+    ];
+  }
+
+  IncreaseCount(): void {
+    this.amountSelected++;
+  }
+
+  DecreaseCount(): void {
+    if (this.amountSelected > 0) this.amountSelected--;
+  }
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+// i need target class that will calculate target stats
+class Stats {
+  totalPoints = 100;
+
+  calc(armorList: Array<Armor>): {
+    healthBoost: number;
+    damageBoost: number;
+    speedBoost: number;
+  } {
+    let tmp = { healthBoost: 1, damageBoost: 1, speedBoost: 1 };
+
+    for (let armor of armorList) {
+      tmp.healthBoost += armor.healthBoost * armor.amountSelected;
+      tmp.damageBoost += armor.damageBoost * armor.amountSelected;
+      tmp.speedBoost += armor.speedBoost * armor.amountSelected;
+    }
+
+    let sum = tmp.damageBoost + tmp.healthBoost + tmp.speedBoost;
+
+    tmp.healthBoost = (tmp.healthBoost / sum) * this.totalPoints;
+    tmp.damageBoost = (tmp.damageBoost / sum) * this.totalPoints;
+    tmp.speedBoost = (tmp.speedBoost / sum) * this.totalPoints;
+
+    return tmp;
+  }
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+//
+@Options({
+  props: {
+    msg: String,
+  },
+})
+//
+export default class HelloWorld extends Vue {
+  // state
+  armor!: Array<Armor>;
+  statistics!: Stats;
+
+  displayedStatistics = {
+    healthBoost: 100 / 3,
+    damageBoost: 100 / 3,
+    speedBoost: 100 / 3,
+  };
+
+  // state (props)
+  msg!: string;
+
+  // functions
+  data(): { armor: Array<Armor>; statistics: Stats } {
+    return {
+      armor: [
+        new Armor("alfa", 1, 2, 1),
+        new Armor("beta", 0, 0, 2),
+        new Armor("gamma", 0, 2, 0),
+        new Armor("delta", 3, 0, 0),
+      ],
+      statistics: new Stats(),
+    };
+  }
+
+  clickMe(): void {
+    this.displayedStatistics = this.statistics.calc(this.armor);
+  }
 }
-a {
-  color: #42b983;
-}
-</style>
+</script> -->
